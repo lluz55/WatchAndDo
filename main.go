be77c9acfbd6ad1296b38388c73f186bee183ebf
lluz55 <lucasluz55@gmail.com>
2019-10-22 20:34:53 -0300

    terminal cli version

--- main.go
+++ main.go
@@ -1,82 +1,54 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"time"
+	"os/exec"
+	"strings"
 
-	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
 	"github.com/inancgumus/screen"
+	"github.com/lluz55/scanfile"
 )
 
 var (
-	port                int  // Server port
-	opensDefaultBrowser bool // Opens browser
-	showQr              bool // Shows QR Code in console to open browser
+	file      string
+	interval  int64
+	waitExist bool
+	command   string
 )
-
-func startServer() {
-	// Create and start a server
-	server := Server{}
-	// TODO: [IMPLEMENT] Usage by command line parsed by flag
-	// TODO: port setter just for debug
-	err := server.StartOnPort(5555)
-	// handles server errors on start up
-	if err != nil {
-		log.Println(err)
-	}
-
-	localAddr := fmt.Sprintf("http://%s:%d", server.addr, server.port)
-
-	fmt.Printf("Listening on %s ...\n", localAddr)
-
-	openBrowser(fmt.Sprintf("%s/wad/", localAddr))
-
-	qr := qrcodeTerminal.New()
-	qr.Get(localAddr).Print()
-
-	// handles asynchronous server errors
-	server.listenErrs = func(err error) {
-		log.Println(err)
-	}
-
-	time.Sleep(5 * time.Second)
-
-	b, _ := json.Marshal(&MessageSend{Type: "watchingnewfile",
-		Payload: &WatchingNewFile{Filename: "Arquivo.txt"},
-	})
-
-	server.SendMsgBytes(b)
-
-}
 
 func main() {
 	screen.Clear()
 	screen.MoveTopLeft()
 
-	// WebSocket mode
-	var wsMode bool
-
-	flag.BoolVar(&wsMode, "ws", false, "For configure and listen on webpage")
-	flag.BoolVar(&opensDefaultBrowser, "o", false, "Opens default browser")
-	flag.BoolVar(&showQr, "qr", false, "Shows QR CODE in console to open default browser")
+	flag.StringVar(&file, "f", "", "Path to file that will be observed")
+	flag.Int64Var(&interval, "i", 3, "Interval to watch file changes")
+	flag.BoolVar(&waitExist, "w", false, "Wait until file be created when it doesn't exists")
+	flag.StringVar(&command, "c", "", "Command that will be executed when file changes")
 
 	flag.Parse()
 
-	// Only work in ws mode for now
-	if !wsMode {
-		fmt.Println("[!] Only web mode implemented yet\n\tExiting...")
-		os.Exit(1)
-	}
+	err := scanfile.WatchFile(file, &scanfile.WatchFileOpts{
+		Interval:   interval,
+		WaitExists: waitExist,
+		Listener: func(msg string, changed bool) {
+			log.Println(msg)
+			if changed {
+				cmds := strings.Split(command, " ")
+				if len(cmds) < 1 {
+					cmds = append(cmds, " ")
+				}
+				cmd := exec.Command(cmds[0], cmds[1:]...)
+				err := cmd.Run()
+				if err != nil {
+					log.Println(err.Error())
+					os.Exit(1)
+				}
+			}
+		},
+	})
 
-	// Start server to connect with listeners
-	startServer()
-
-	for {
-		time.Sleep(24 * time.Hour)
-	}
+	log.Println(err.Error())
 
 }
